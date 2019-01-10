f1c3d0b284a1ecb85955c800e96912c3385de866
PcoleDemos <44314077+PcoleDemos@users.noreply.github.com>
2019-01-10 15:16:03 +1100

    updated code
    removed wheel

--- QRGenerator.py
+++ QRGenerator.py
@@ -1,4 +1,4 @@
---find-links wheelhouse
-pypng==0.0.19
-pyqrcode==1.2.1
-import pyqrcode >>> qr = pyqrcode.create('Unladden swallow') >>> qr.png('famous-joke.png', scale=5)
+
+import pyqrcode
+qr = pyqrcode.create('Unladden swallow')
+qr.png('famous-joke.png', scale=5)
